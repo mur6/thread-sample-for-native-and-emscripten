2e641c2d9437ee61f4567bbf3213ade2756c1882
MURAKI Taichi <muraki.taichi@gmail.com>
2024-12-09 12:54:05 +0900

    asyncとembindのテスト.

--- make_em_test2.sh
+++ make_em_test2.sh
@@ -10,6 +10,8 @@ clean() {
 build() {
     mkdir -p $BUILD_DIR
     emcc src/test2/main.cpp -pthread \
+        --bind \
+        --emrun \
         -s EXIT_RUNTIME=0 \
         -s PROXY_TO_PTHREAD=1 \
         -s PTHREAD_POOL_SIZE=$PTHREAD_POOL_SIZE \
--- src/test2/main.cpp
+++ src/test2/main.cpp
@@ -1,10 +1,33 @@
 #include <future>
 #include <iostream>
 
-int main()
+#include <emscripten/bind.h>
+#include <emscripten/val.h>
+#include <emscripten.h>
+
+int calc()
 {
     std::future<int> fut = std::async(std::launch::async, [] { return 42; });
-    auto result = fut.get();
-    std::cout << "The answer is " << result << std::endl;
-    return 0;
+    return fut.get();
+}
+
+EMSCRIPTEN_BINDINGS(my_module) {
+    emscripten::function("calc", &calc);
+}
+
+EMSCRIPTEN_KEEPALIVE
+int main(int arg, char** argv){
+    // print hello world
+    std::cout << "Hello World!" << std::endl;
+    std::cout << "This is a C++ program: argc=" << arg << std::endl;
+    for (int i = 0; i < arg; i++) {
+        std::cout << "argv[" << i << "]: " << argv[i] << std::endl;
+    }
+
+  EM_ASM(
+    console.log('hello world!, this is console.log from EM_ASM');
+    const r = Module.calc();
+    console.log('Module.calc=', r);
+  );
+  return 0;
 }
