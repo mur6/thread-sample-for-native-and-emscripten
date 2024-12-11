441f381d79c9bc0e16f981b47aa5895660904539
MURAKI Taichi <muraki.taichi@gmail.com>
2024-12-11 16:17:01 +0900

    fix

--- tests/20241211/build.sh
+++ tests/20241211/build.sh
@@ -6,21 +6,37 @@ clean() {
     echo "DIR=$HERE"
     echo "Cleaning..."
     rm -rf $HERE/build
+    rm -rf $HERE/web
 }
 
-build() {
+build_native() {
     echo "DIR=$HERE"
     echo "Building..."
     mkdir -p $HERE/build
     clang++ $HERE/src/cpp/main.cpp -O1 -std=c++20 -o $HERE/build/main
 }
 
-run() {
+build_em() {
+    echo "DIR=$HERE"
+    echo "Building..."
+    mkdir -p $HERE/web
+    emcc $HERE/src/cpp/main.cpp -std=c++20 -o $HERE/web/main.js
+}
+
+run_native() {
     echo "DIR=$HERE"
     echo "Running..."
     $HERE/build/main
 }
 
-clean
-build
-run
+native_all() {
+    clean
+    build_native
+    run_native
+}
+
+em_all() {
+    echo "DIR=$HERE"
+    echo "Running..."
+    emrun --browser chrome $HERE/web/main.js
+}
