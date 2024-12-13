a61cc6e55cce52b4e7e4a3114bce6902a1375d02
MURAKI Taichi <muraki.taichi@gmail.com>
2024-12-13 15:26:36 +0900

    スレッド関係の新しい実験用ディレクトリ.

--- tests/20241213/build.sh
+++ tests/20241213/build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 HERE=$(dirname $(realpath $0))
+BUILD_DIR=$HERE/web/dist
 
 _clean() {
     target_dir=$1
@@ -9,39 +10,19 @@ _clean() {
 }
 
 clean_em() {
-    target_dir=$HERE/web/dist
-    _clean "$target_dir"
+    _clean $BUILD_DIR
 }
 
 build_em() {
     echo "DIR=$HERE"
     echo "Building..."
-    mkdir -p $HERE/web
-    emcc $HERE/src/cpp/main.cpp -std=c++20 -o $HERE/web/main.js
+    mkdir -p "BUILD_DIR"
+    echo "BUILD_DIR=$BUILD_DIR"
+    emcc $HERE/src/cpp/main.cpp -std=c++20 -o $BUILD_DIR/main.js
 }
 
-
-build_native() {
-    echo "DIR=$HERE"
-    echo "Building..."
-    mkdir -p $HERE/build
-    clang++ $HERE/src/cpp/main.cpp -O1 -std=c++20 -o $HERE/build/main
-}
-
-run_native() {
+run_em() {
     echo "DIR=$HERE"
     echo "Running..."
-    $HERE/build/main
-}
-
-native_all() {
-    clean
-    build_native
-    run_native
-}
-
-em_all() {
-    echo "DIR=$HERE"
-    echo "Running..."
-    emrun --browser chrome $HERE/web/main.js
+    # emrun --browser chrome $HERE/web/main.js
 }
