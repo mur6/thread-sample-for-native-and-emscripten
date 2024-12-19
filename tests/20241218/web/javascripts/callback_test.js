0483316170f7204b7f95f0cfb7d803beca267071
MURAKI Taichi <muraki.taichi@gmail.com>
2024-12-19 14:10:13 +0900

    fix

--- tests/20241218/web/javascripts/callback_test.js
+++ tests/20241218/web/javascripts/callback_test.js
@@ -1,1 +1,39 @@
+// use comlink to run heavyCalc() in a worker
+
 console.log('callback_test.js loaded');
+
+// function fib(n) {
+//     if (n < 2) {
+//         return n;
+//     }
+//     return fib(n - 1) + fib(n - 2);
+// }
+
+// memoized version
+const memo = new Map();
+
+function fib(n) {
+    if (n < 2) {
+        return n;
+    }
+    if (memo.has(n)) {
+        return memo.get(n);
+    }
+    const result = fib(n - 1) + fib(n - 2);
+    memo.set(n, result);
+    return result;
+}
+
+function heavyCalc(n) {
+    return fib(n);
+}
+
+const run = async () => {
+    const time = new Date().getTime();
+    console.log("heavyCalc(): start");
+    const result = heavyCalc(45);
+    const elapsed = new Date().getTime() - time;
+    console.log(`heavyCalc(): end, result=${result}, elapsed=${elapsed}ms`);
+}
+
+run();
