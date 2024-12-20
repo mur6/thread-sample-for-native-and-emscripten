e5cb25e3a2db7922f39413db11d27591c27e3c42
MURAKI Taichi <muraki.taichi@gmail.com>
2024-12-20 15:36:41 +0900

    fix

--- tests/20241220/web/javascripts/test02/entry.js
+++ tests/20241220/web/javascripts/test02/entry.js
@@ -3,9 +3,11 @@ console.log("javascripts/test02/entry.js loaded");
 import loadWASM from '/dist/em_heavy_calculation.js';
 
 let Module = null;
+let calcResult = 0;
 
 const onCalcComplete = (retValue) => {
     console.log(`onCalcComplete: retValue=${retValue}`);
+    calcResult = retValue;
 };
 
 const initModule = async () => {
@@ -14,31 +16,53 @@ const initModule = async () => {
             Module = await loadWASM();
             console.log("Module created", Module);
         }
-        Module["onCalcComplete"] = (retValue) => {
-            console.log(`onCalcComplete: retValue=${retValue}`);
-        }
     } catch (error) {
         console.error("Error loading or running WASM:", error); // Catch and log the error
     }
 }
 
-const main = async () => {
+initModule();
+
+async function calc_fib(n) {
     await initModule();
-    console.log("main() start");
-    const result = Module.startCalculation(20);
-    console.log("main() end", result);
+    Module.startCalculation(n);
+    return new Promise((resolve, reject) => {
+        const interval = setInterval(() => {
+            if (calcResult !== 0) {
+                const result = calcResult;
+                clearInterval(interval);
+                resolve(result);
+            }
+        });
+    });
 }
 
-main();
+const registerEvents = () => {
+    // <div>
+    //     <h2>フィボナッチ数列を計算する</h2>
+    //     <input type="number" id="fib-input-num" value="10" />
+    //     <button id="fib-calc">計算</button>
+    // </div>
+    document.getElementById("fib-calc").addEventListener("click", async () => {
+        const num_str = document.getElementById("fib-input-num").value;
+        const num = parseInt(num_str, 10);
+        console.log(`fib-calc: num=${num}`);
+        
+    });
 
-let counter = 0;
-document.getElementById("increment").addEventListener("click", () => {
-    // <p id="output-counter">0</p>
-    counter++;
-    document.getElementById("output-counter").innerText = counter;
-});
-document.getElementById("decrement").addEventListener("click", () => {
-    // <p id="output-counter">0</p>
-    counter--;
-    document.getElementById("output-counter").innerText = counter;
-});
+    // ダミーの処理: カウンター
+    let counter = 0;
+    document.getElementById("increment").addEventListener("click", () => {
+        // <p id="output-counter">0</p>
+        counter++;
+        document.getElementById("output-counter").innerText = counter;
+    });
+    document.getElementById("decrement").addEventListener("click", () => {
+        // <p id="output-counter">0</p>
+        counter--;
+        document.getElementById("output-counter").innerText = counter;
+    });
+
+}
+
+registerEvents();
