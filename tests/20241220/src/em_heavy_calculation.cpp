da85b40f363de017bd911bd3dbb7994af4905c4e
MURAKI Taichi <muraki.taichi@gmail.com>
2024-12-20 16:16:26 +0900

    fix

--- tests/20241220/src/em_heavy_calculation.cpp
+++ tests/20241220/src/em_heavy_calculation.cpp
@@ -24,8 +24,8 @@ int heavy_computation(int n) {
 
 void heavy_calculation_wrapped(int n) {
     calculation_complete.store(false);
-    int result = heavy_computation(n);
-    result.store(result);
+    int r = heavy_computation(n);
+    result.store(r);
     calculation_complete.store(true);
 }
 
@@ -38,6 +38,25 @@ void heavy_calculation_wrapped(int n) {
 //     return result.load();
 // }
 
+void check_status_and_call_js_callback() {
+    if (calculation_complete.load()) {
+        std::cout << "check_status_and_call_js_callback: 計算終了." << std::endl;
+        int r = result.load();
+        EM_ASM({
+            // check Module has onCalcComplete function
+            if (window.onCalcComplete) {
+                window.onCalcComplete($0);
+            } else {
+                console.error('window.onCalcComplete is not defined');
+            }
+        }, r);
+    } else {
+        std::cout << "check_status_and_call_js_callback: 計算中..." << std::endl;
+    }
+    emscripten_async_call([](void*){
+        check_status_and_call_js_callback();
+    }, nullptr, 500); // 0.5秒ごとに呼び出し
+}
 
 void start_calculation(int n) {
     std::cout << "start_calculation: 計算開始: " << n << std::endl;
@@ -47,8 +66,8 @@ void start_calculation(int n) {
         heavy_calculation_wrapped(n);
         std::cout << "thread: 計算終了." << std::endl;
     }).detach();
+    check_status_and_call_js_callback();
 }
-
 
 EMSCRIPTEN_BINDINGS(module) {
     emscripten::function("startCalculation", &start_calculation);
