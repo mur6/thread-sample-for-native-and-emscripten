eca73168146d8b65e122ea8ddb55ed41f71d8b76
MURAKI Taichi <muraki.taichi@gmail.com>
2024-12-20 11:18:39 +0900

    fix

--- tests/20241220/web/javascripts/test01/entry.js
+++ tests/20241220/web/javascripts/test01/entry.js
@@ -1,30 +1,6 @@
-import loadWASM from '/dist/em_thread_and_event_loop.js';
-console.log("entry.js loaded");
+console.log("test01/entry.js loaded");
 
-const calcAsync = async (Module) => {
-    Module.startCalculation();
-
-    return new Promise((resolve, reject) => {
-        const address = Module.get_calculation_complete_address(); // アドレスを取得
-        const HEAP8 = Module.HEAP8;
-        const interval = setInterval(() => {
-            const value = Atomics.load(HEAP8, address);
-            const msg = `calculation_complete Status: ${value ? "Done" : "In Progress"}`;
-            console.log(msg);
-            if (value) {
-                const result = Module.getCalculationResult();
-                console.log('Calculation result:', result);
-                clearInterval(interval);
-                resolve(result);
-            } else {
-                const unfinishedResult = Module.getCalculationResult();
-                console.log(`unfinishedResult=${unfinishedResult}`);
-            }
-        }, 1000);
-    });
-}
-
-
+const worker = new Worker("./worker.js");
 
 const run = async () => {
     try{
@@ -33,22 +9,6 @@ const run = async () => {
         const result = await calcAsync(Module);
         console.log(`Module.startCalculation(): end, result=${result}`);
         document.getElementById("output-result").innerText = result;
-
-        // const interval = setInterval(() => {
-        //     console.log('interval test.');
-        // }, 1000);
-        // const address = Module.get_calculation_complete_address(); // アドレスを取得
-        // const HEAP8 = new Int8Array(Module.HEAP8.buffer);
-        // const interval2 = setInterval(() => {
-        //     const value = Atomics.load(HEAP8, address);
-        //     const msg = `calculation_complete Status: ${value ? "Done" : "In Progress"}`;
-        //     console.log(msg);
-        //     if (value) {
-        //         console.log('Calculation result:', Module.getCalculationResult());
-        //         clearInterval(interval2);
-        //     }
-        // }, 1000);
-
 
     } catch (error) {
         console.error("Error loading or running WASM:", error); // Catch and log the error
--- tests/20241220/web/javascripts/test01/worker.js
+++ tests/20241220/web/javascripts/test01/worker.js
@@ -1,49 +1,14 @@
-import loadWASM from '/dist/main.js';
-
-let Module = null;
-
+const add = (a, b) => a + b;
 
 onmessage = async function (e) {
-    console.log("Message received from main script");
     const { funcName, input } = e.data;
     console.log(`recieved: funcName=${funcName}`);
     console.log(`recieved: input=${input}`);
 
-    if (!Module) {
-        Module = await loadWASM();
-    }
-
-    if (funcName === "appendAndSumOfAll") {
-        const inputNums = input;
-        console.log(`inputNums=${inputNums}`);
-        const vectorInt = await Module.convertJSArrayToVector(inputNums);
-        console.log(`vectorInt=${vectorInt}`);
-        const result = Module.appendAndSumOfAll(vectorInt);
-        postMessage({ result: result });
-    } else if (funcName === "heavyCalc") {
-        const result = await Module.heavyCalc();
+    if (funcName === "add") {
+        const result = add(input.a, input.b);
         postMessage({ result: result });
     } else {
         postMessage({ error: `unknown funcName=${funcName}` });
     }
 };
-
-
-// onmessage = async function (e) {
-//     console.log("Message received from main script");
-//     const { funcId, input } = e.data;
-
-//     try {
-//         const Module = await loadWASM();
-//         console.log(Module);
-
-//         const result = await Module.calc();
-//         console.log(`calc result=${result}`);
-
-//         // Promiseではなく、実際の結果値を送信
-//         postMessage({ result });
-//     } catch (error) {
-//         console.error('Error in worker:', error);
-//         postMessage({ error: error.toString() });
-//     }
-// };
