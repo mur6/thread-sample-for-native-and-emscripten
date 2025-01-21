09f1bf35d8380586b41673bbb09e130b59237fde
MURAKI Taichi <muraki.taichi@gmail.com>
2025-01-21 10:03:05 +0900

    fix

--- tests/20250121/web/save_as_png/main.js
+++ tests/20250121/web/save_as_png/main.js
@@ -3,6 +3,63 @@ console.log("save_as_png/ fs of emscripten test");
 import loadWASM from '/save_as_png/dist/my_em.js';
 import { showFileList } from '/save_as_png/em_fs.js';
 
+
+async function initVideo(init_params, Module) {
+    const video = document.getElementById('videoElement');
+    const canvas = document.getElementById('videoCanvas');
+    const outputCanvas = document.getElementById('outputCanvas');
+    outputCanvas.width = 720;
+    outputCanvas.height = 1280;
+    const captureButton = document.getElementById('captureButton');
+    const processor = new Module.ImageProcessor();
+
+    navigator.mediaDevices.getUserMedia({
+        video: {facingMode: 'environment'},
+        audio:false,
+    }).then(stream => {
+        video.srcObject = stream;
+    });
+
+    captureButton.addEventListener('click', () => {
+        // Draw video frame to canvas
+        canvas.width = video.videoWidth;
+        canvas.height = video.videoHeight;
+        const context = canvas.getContext('2d');
+        context.drawImage(video, 0, 0, canvas.width, canvas.height);
+
+        // Get image data from canvas
+        const imageData = context.getImageData(0, 0, canvas.width, canvas.height);
+        // C++で画像を処理
+        const processedData = processImage(
+            imageData.data,
+            canvas.width,
+            canvas.height
+        );
+
+        // Call the C++ function using embind
+        // Module.simpleTest();
+        // console.log('Module.BuildAndReturnFootModel=', Module.BuildAndReturnFootModel);
+        // console.log('Module.processCameraImage=', Module.processCameraImage);
+        // Module.processCameraImage(inputImage, canvas.width, canvas.height).then(resizedImage => {
+        //     // Create ImageData from the returned vector
+        //     const resultImageData = new ImageData(new Uint8ClampedArray(resizedImage), 720, 1280);
+
+        //     // Draw the processed image to the canvas
+        //     outputCanvas.width = 720;
+        //     outputCanvas.height = 1280;
+        // });
+        Module.SaveAsPngFromUint8Array("test.png", processedData);
+        const processedImageData = new ImageData(
+            new Uint8ClampedArray(processedData),
+            720,
+            1280
+        );
+        const ctx = outputCanvas.getContext('2d');
+        ctx.putImageData(processedImageData, 0, 0);
+        // const result = validate(init_params, processedData, Module);
+    });
+}
+
 const run = async () => {
     // Load the wasm module and get the exports
     const wasmModule = await loadWASM();
